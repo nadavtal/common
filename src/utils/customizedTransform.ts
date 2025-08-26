b514a89da66fcbb34fe6f74a0b0441852bd463d4
nadavtal <nadavtalalmagor@gmail.com>
2025-08-26 12:28:00 +0300

    Updates

--- src/utils/customizedTransform.ts
+++ src/utils/customizedTransform.ts
@@ -1,21 +1,7 @@
 'use strict'
-import  allJson from '../data/all.json';
-console.log('allJson', allJson);
-export type EPSGType = {
-  [key: string]: {
-    code: string;
-    kind: string;
-    name: string;
-    wkt: string | null;
-    proj4: string | null;
-    bbox: number[];
-    unit: string;
-    area: string;
-    accuracy: number | null; // <-- allow null
-  };
-};
 
-const epsg =  allJson as EPSGType
+import  allJson from '../data/all.json';
+const epsg = allJson
 const proj4 = require('proj4')
 
 const leadingEPSG = /^epsg:/i
@@ -23,11 +9,13 @@ const leadingEPSG = /^epsg:/i
 const customizedTransform = (from: string, to: string) => {
 	if ('string' !== typeof from) throw new Error('from must be a string')
 	from = from.replace(leadingEPSG, '')
+// @ts-ignore
 	const fromEPSG = epsg[from]
 	if (!fromEPSG) throw new Error(from + ' is not a valid EPSG coordinate system')
 
 	if ('string' !== typeof to) throw new Error('to must be a string')
 	to = to.replace(leadingEPSG, '')
+// @ts-ignore
 	const toEPSG = epsg[to]
 	if (!toEPSG) throw new Error(to + ' is not a valid EPSG coordinate system')
 
